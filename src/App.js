5cc9fc9f72ed49013f7d7653839482148ac0be7d
Sam <sambisselele@gmail.com>
2022-07-31 22:08:35 +0100

    Feat(Adding primary style to body)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home"
 import QuestionsPage from "./pages/QuestionsPage"
